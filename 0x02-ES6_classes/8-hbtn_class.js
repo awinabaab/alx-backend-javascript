0b51bbd64716d34a11f1284174d0dd403993aa41
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-04-17 12:18:16 +0000

    Add 8-hbtn_class.js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,18 @@
+export default class ALXClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](type) {
+    if (type === 'number') {
+      return this._size;
+    }
+
+    if (type === 'string') {
+      return this._location;
+    }
+
+    return this;
+  }
+}
