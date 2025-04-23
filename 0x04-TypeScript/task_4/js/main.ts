61fc8c42f110d3c95d8eb41dbad8634afd9d92da
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-04-23 21:25:59 +0000

    feat (task_4): add solutions to task_4

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,29 @@
+/// <reference path='subjects/Cpp.ts' />
+/// <reference path='subjects/React.ts' />
+/// <reference path='subjects/Java.ts' />
+/// <reference path='subjects/Teacher.ts' />
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  experienceTeachingC: 10,
+};
+
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+cpp.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+cpp.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,22 @@
+/// <reference path='Subject.ts' />
+/// <reference path='Teacher.ts' />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requrements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+
+      return 'No available teacher';
+    }
+  }
+}
